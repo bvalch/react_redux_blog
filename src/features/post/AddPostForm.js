b71baf9930c90bd613e7cf6da8135dec29bce807
bvalch <bvalch@gmail.com>
2023-01-09 20:22:41 +0000

    save new post functionality added

--- src/features/post/AddPostForm.js
+++ src/features/post/AddPostForm.js
@@ -1,10 +1,30 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+//nanoid generates random id's
+import { nanoid } from "@reduxjs/toolkit";
+import { postAdded } from "./postsSlice";
+
 const AddPostForm=()=>{
     const [title,setTitle]=useState("");
     const [content,setContent]=useState("");
 
+
     const handleTitleChange=e=>setTitle(e.target.value)
     const handleContentChange=e=>setContent(e.target.value)
+    const handleSubmitPost=()=>{
+        if(title&&content){
+            dispatch(
+                postAdded({
+                    id:nanoid(),
+                    title,
+                    content
+                })
+            )
+            setTitle("")
+            setContent("")
+        }
+    }
+
     return(
       <section>
         <h2>Add a New Post</h2>
@@ -24,6 +44,7 @@ const AddPostForm=()=>{
             value={content}
             onChcange={handleContentChange}
             />
+            <button type="button" onClick={handleSubmitPost}>Save Post</button>
         </form>
 
       </section>
