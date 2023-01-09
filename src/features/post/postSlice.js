6385c6d178b6e8c254e24bca5fe40a183c00daf8
bvalch <bvalch@gmail.com>
2023-01-09 19:54:23 +0000

    project skeleton

---
+++ src/features/post/postSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [
+  { id: "1", title: "Redux toolkit ex", content: "Time to learn more I guess" },
+  { id: "2", title: "Another title", content: "Some text for content" },
+];
+const postsSlice=createSlice({
+    name:"posts",
+    initialState,
+    reducers:{
+
+    }
+});
+
+export default postsSlice.reducer;
