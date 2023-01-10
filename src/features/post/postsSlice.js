c841f96cd7ce203917ebaf98dcb5f1688da3ef8e
bvalch <bvalch@gmail.com>
2023-01-10 01:45:43 +0000

    added reaction buttons

--- src/features/post/postsSlice.js
+++ src/features/post/postsSlice.js
@@ -9,6 +9,13 @@ const initialState = [
     title: "Redux toolkit ex",
     content: "Time to learn more I guess",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions:{
+      thumbsUp:0,
+      wow:0,
+      heart:0,
+      rocket:0,
+      coffee:0
+    }
   },
 
   {
@@ -16,6 +23,13 @@ const initialState = [
     title: "Another title",
     content: "Some text for content",
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions:{
+      thumbsUp:0,
+      wow:0,
+      heart:0,
+      rocket:0,
+      coffee:0
+    }
   },
 ];
 
@@ -35,13 +49,27 @@ const postsSlice = createSlice({
             content,
             date:(new Date()).toISOString(),
             userId,
+            reactions:{
+              thumbsUp:0,
+              wow:0,
+              heart:0,
+              rocket:0,
+              coffee:0
+            }
           },
         };
       },
     },
+    reactionAdded(state,action){
+      const {postId,reaction}=action.payload
+      const existingPost=state.find(post=>post.id===postId)
+      if(existingPost){
+        existingPost.reactions[reaction]++
+      }
+    }
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded,reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
